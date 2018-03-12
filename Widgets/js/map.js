d181dbae66a6bfc1eea83b2c0a7825aeeeaddcc4
GovTech_MAC_05 <govtech_mac_05@GovTech-MAC-05s-MacBook-Pro.local>
2018-03-12 13:26:16 +0800

    Update missing code

--- Widgets/js/map.js
+++ Widgets/js/map.js
@@ -6,13 +6,15 @@ require([
         "esri/geometry/Extent",
         "esri/layers/ArcGISDynamicMapServiceLayer",
         "esri/layers/FeatureLayer",
+        "esri/dijit/BasemapToggle",
+        "esri/dijit/Legend",
         "dojo/ready",
         "dojo/parser",
         "dojo/on",
 
         "dijit/layout/BorderContainer",
         "dijit/layout/ContentPane"],
-    function (Map, Extent,ArcGISDynamicMapServiceLayer,FeatureLayer,
+    function (Map, Extent,ArcGISDynamicMapServiceLayer,FeatureLayer,BasemapToggle,Legend,
               ready, parser, on,
               BorderContainer, ContentPane) {
 // @formatter:on
@@ -51,31 +53,42 @@ require([
             var lyrUSA = new ArcGISDynamicMapServiceLayer("https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer", {
                 opacity : 0.5
             });
-            mapMain.addLayer(lyrUSA);
+            // mapMain.addLayer(lyrUSA);
 
 
             /*
              * Step: Add the earthquakes layer to the map
              */
-            var lyrQuakes = new FeatureLayer("");
+            var lyrQuakes = new FeatureLayer("https://services.arcgis.com/ue9rwulIoeLEI9bj/arcgis/rest/services/Earthquakes/FeatureServer/0");
             lyrQuakes.setDefinitionExpression("MAGNITUDE >= 2.0");
-            mapMain.addLayer(lyrQuakes);
+            // mapMain.addLayer(lyrQuakes);
 
 
             /*
             * Step: Revise code to use the addLayers() method
             */
+            mapMain.addLayers([lyrUSA, lyrQuakes]);
 
             /*
              * Step: Add the BaseMapToggle widget to the map
              */
-
+             var toggle = new BasemapToggle({
+                  map: mapMain,
+                  basemap: "topo"
+              }, "BasemapToggle");
+              toggle.startup();
 
             /*
              * Step: Add a legend once all layers have been added to the map
              */
             //mapMain.on(); // stub
-
+            mapMain.on("layers-add-result", function() {
+              var dijitLegend = new Legend({
+                map : mapMain,
+                arrangement : Legend.ALIGN_RIGHT
+              }, "divLegend");
+              dijitLegend.startup();
+            }); // stub)
 
         });
     });
